8634b2b15331df6d3fab4819e48e296670549247
YURII <trohumchyk@gmail.com>
2015-08-24 21:26:46 +0300

    add sudent model

---
+++ TYVStudents/TYVStudents/Models/TYVSudent/TYVSudent.h
@@ -0,0 +1,20 @@
+//
+//  TYVSudent.h
+//  TYVStudents
+//
+//  Created by YURII on 24.08.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TYVSudent : NSObject
+@property (nonatomic, copy) NSString    *name;
+@property (nonatomic, copy) NSString    *photoName;
+
++ (instancetype)sudent;
+
+- (instancetype)initWithName:(NSString *)name
+                   photoName:(NSString *)photoName;
+
+@end
---
+++ TYVStudents/TYVStudents/Models/TYVSudent/TYVSudent.m
@@ -0,0 +1,13 @@
+//
+//  TYVSudent.m
+//  TYVStudents
+//
+//  Created by YURII on 24.08.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#import "TYVSudent.h"
+
+@implementation TYVSudent
+
+@end
