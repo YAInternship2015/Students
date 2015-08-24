8f181e580dd2d2b9d6a62e5d4fce9285231ed047
YURII <trohumchyk@gmail.com>
2015-08-24 21:26:35 +0300

    create sudents

---
+++ TYVStudents/TYVStudents/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TYVStudents
+//
+//  Created by YURII on 24.08.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TYVStudents/TYVStudents/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TYVStudents
+//
+//  Created by YURII on 24.08.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
