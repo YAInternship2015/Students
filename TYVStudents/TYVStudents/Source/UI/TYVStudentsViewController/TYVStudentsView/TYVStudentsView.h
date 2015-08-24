052971604325ff117f11c0f670522d80589cd0ef
YURII <trohumchyk@gmail.com>
2015-08-24 22:07:20 +0300

    add controller add mainView

---
+++ TYVStudents/TYVStudents/Source/UI/TYVStudentsViewController/TYVStudentsView/TYVStudentsView.h
@@ -0,0 +1,14 @@
+//
+//  TYVStudentsView.h
+//  TYVStudents
+//
+//  Created by YURII on 24.08.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TYVStudentsView : UIView
+@property (nonatomic, strong)   IBOutlet UITableView    *tableView;
+
+@end
---
+++ TYVStudents/TYVStudents/Source/UI/TYVStudentsViewController/TYVStudentsViewController.m
@@ -0,0 +1,22 @@
+//
+//  TYVStudentsViewController.m
+//  TYVStudents
+//
+//  Created by YURII on 24.08.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#import "TYVStudentsViewController.h"
+#import "TYVStudentsView.h"
+
+#import "TYVMacro.h"
+
+TYVViewControllerProperty(TYVStudentsViewController, rootView, TYVStudentsView)
+
+@interface TYVStudentsViewController ()
+
+@end
+
+@implementation TYVStudentsViewController
+
+@end
